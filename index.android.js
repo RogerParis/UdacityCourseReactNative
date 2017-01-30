8663c48543255fe3138a6d94895298ac5fe3c9cf
RogerParis <roger.parisvicent@gmail.com>
2017-01-30 16:39:17 +0100

    first commit

---
+++ index.android.js
@@ -0,0 +1,15 @@
+// Index.android.js place Android code
+
+// Import a library to help create a component
+import React from 'react';
+import { AppRegistry } from 'react-native';
+import Header from './src/components/Header';
+
+
+// Create a component
+const App = () => (
+    <Header headerText={'Albums'} />
+);
+
+// Render it to the device
+AppRegistry.registerComponent('UdemyCourseReact', () => App);
